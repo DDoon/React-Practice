8fc5d77d8fe74320a0a2896e3b485c05408f28d7
Mong9 <bean326@naver.com>
2022-08-28 19:45:51 +0900

    ♻️ Refactor: AddUser State obj

--- Project-Practice/01-starting-project/src/components/Users/AddUser.js
+++ Project-Practice/01-starting-project/src/components/Users/AddUser.js
@@ -4,28 +4,45 @@ import Button from '../UI/Button'
 import Card from '../UI/Card'
 import classes from './AddUser.module.css'
 const AddUser = (props) => {
-  const [enteredUserName, setEnteredUserName] = useState('')
-  const [enteredAge, setEnteredAge] = useState('')
+  const [enteredUserInfo, setEnteredUserInfo] = useState({
+    username: '',
+    age: 0,
+  })
   const addUserHandler = (e) => {
     e.preventDefault()
-    console.log(enteredUserName, enteredAge)
+    console.log(enteredUserInfo)
+    setEnteredUserInfo({
+      username: '',
+      age: '',
+    })
   }
 
-  const userNameChangeHandler = (e) => {
-    setEnteredUserName(e.target.value)
-  }
-
-  const userAgeChangeHandler = (e) => {
-    setEnteredAge(e.target.value)
+  const userInfoChangeHandler = (e) => {
+    setEnteredUserInfo({
+      ...enteredUserInfo,
+      [e.target.id]: e.target.value,
+    })
   }
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input onChange={userNameChangeHandler} id="username" type="text" />
+        <input
+          value={enteredUserInfo.username}
+          onChange={userInfoChangeHandler}
+          id="username"
+          type="text"
+          name="username"
+        />
         <label htmlFor="age">Age(Years)</label>
-        <input onChange={userAgeChangeHandler} id="age" type="Number" />
+        <input
+          value={enteredUserInfo.age}
+          onChange={userInfoChangeHandler}
+          id="age"
+          type="Number"
+          name="age"
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>

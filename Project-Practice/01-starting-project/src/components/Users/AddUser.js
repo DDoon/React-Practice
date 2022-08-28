fd64b3be9c955fadf92075694192ba27b666094a
Mong9 <bean326@naver.com>
2022-08-28 19:38:01 +0900

    ✅ feat: Add AddUser  username, age useState

--- Project-Practice/01-starting-project/src/components/Users/AddUser.js
+++ Project-Practice/01-starting-project/src/components/Users/AddUser.js
@@ -1,18 +1,31 @@
+import { useState } from 'react'
+
 import Button from '../UI/Button'
 import Card from '../UI/Card'
 import classes from './AddUser.module.css'
 const AddUser = (props) => {
+  const [enteredUserName, setEnteredUserName] = useState('')
+  const [enteredAge, setEnteredAge] = useState('')
   const addUserHandler = (e) => {
     e.preventDefault()
+    console.log(enteredUserName, enteredAge)
+  }
+
+  const userNameChangeHandler = (e) => {
+    setEnteredUserName(e.target.value)
+  }
+
+  const userAgeChangeHandler = (e) => {
+    setEnteredAge(e.target.value)
   }
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input onChange={userNameChangeHandler} id="username" type="text" />
         <label htmlFor="age">Age(Years)</label>
-        <input id="age" type="Number" />
+        <input onChange={userAgeChangeHandler} id="age" type="Number" />
         <Button type="submit">Add User</Button>
       </form>
     </Card>

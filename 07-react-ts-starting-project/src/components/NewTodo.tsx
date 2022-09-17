f740d11f5dd758d3da6c617d9a71c5f72fedf577
DDoon <bean326@naver.com>
2022-09-17 18:17:03 +0900

    ✅ 22/09/17

--- 07-react-ts-starting-project/src/components/NewTodo.tsx
+++ 07-react-ts-starting-project/src/components/NewTodo.tsx
@@ -1,21 +1,28 @@
-import { useRef, FormEvent } from 'react'
-const NewTodo = () => {
+import { useRef, useContext } from 'react'
+
+import { TodosContext } from '../store/todos-context'
+import classes from './NewTodo.module.css'
+
+const NewTodo: React.FC = () => {
+  const todosCtx = useContext(TodosContext)
+
   const todoTextInputRef = useRef<HTMLInputElement>(null)
 
-  const submitHandler = (event: FormEvent) => {
+  const submitHandler = (event: React.FormEvent) => {
     event.preventDefault()
 
-    const enteredText = todoTextInputRef.current?.value
+    const enteredText = todoTextInputRef.current!.value
 
     if (enteredText.trim().length === 0) {
       // throw an error
-
       return
     }
+
+    todosCtx.addTodo(enteredText)
   }
 
   return (
-    <form onSubmit={submitHandler}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <label htmlFor="text">Todo text</label>
       <input type="text" id="text" ref={todoTextInputRef} />
       <button>Add Todo</button>
--- 07-react-ts-starting-project/src/components/Todos.tsx
+++ 07-react-ts-starting-project/src/components/Todos.tsx
@@ -1,29 +1,23 @@
-import React from 'react'
-import Todo from '../models/todo'
-import TodoItem from './TodoItem'
+import React, { useContext } from 'react'
 
-const Todos: React.FC<{ items: Todo[] }> = (props) => {
+import TodoItem from './TodoItem'
+import { TodosContext } from '../store/todos-context'
+import classes from './Todos.module.css'
+
+const Todos: React.FC = () => {
+  const todosCtx = useContext(TodosContext)
+
   return (
-    <ul>
-      {props.items.map((item) => (
-        <TodoItem key={item.id} text={item.text} />
+    <ul className={classes.todos}>
+      {todosCtx.items.map((item) => (
+        <TodoItem
+          key={item.id}
+          text={item.text}
+          onRemoveTodo={todosCtx.removeTodo.bind(null, item.id)}
+        />
       ))}
     </ul>
   )
 }
 
 export default Todos
-
-// props는 객체이고 items는 key이며 문자열 배열을 값으로 갖는다
-// 하지만 객체에는 항상 특별한 프로퍼티(prop)이 있음
-// children 프로퍼티 타입을 모르지
-// 함수형 컴포넌트를 바로 제네릭 함수로 변환해서 사용
-// FC는 함수형 컴포넌트 (Functional Component)
-// FC는 이미 제네릭타입이다
-
-// React.FC<>는 항상 children을 가질 수 있다.
-// 컴포넌트에 children이 있을 수 있다는 것을 의미
-
-// React.FC를 쓰는 것이 꼭 나쁘지만은 않지만
-// props를 타이핑 하는 것이 조금 더 자바스크립트의 느낌과 비슷하고,
-// 다양한 경우의 수로 부터 족므 더 안전해 질수 있다.
